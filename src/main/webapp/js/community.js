1980138f70b8d0eaf5c67a7ffe82f08637dd6cfe
Van <lly219@gmail.com>
2014-03-13 15:02:45 +0800

    c

---
+++ src/main/webapp/js/community.js
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2014, XinZhuBang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * @fileOverview 社区
+ *
+ * @author <a href="mailto:LLY219@gmail.com">Liyuan Li</a>
+ * @version 1.0.0.1, Mar 13, 2014
+ */
+var community = {
+    salePublish: function() {
+        if ($.trim($("#title").val()).length === 0) {
+            tip.show("提交失败", "标题不能为空");
+        } else if ($.trim($("#details").val()).length === 0) {
+            tip.show("提交失败", "描述不能为空");
+        } else if ($.trim($("#money").val()).length === 0) {
+            tip.show("提交失败", "价格不能为空");
+        } else {
+            var requestJSONObject = {
+                title: $("#title").val(),
+                details: $("#details").val(),
+                money: $("#money").val()
+            };
+
+            $.ajax({
+                url: "/login",
+                type: "POST",
+                cache: false,
+                data: JSON.stringify(requestJSONObject),
+                success: function(result, textStatus) {
+                    if (result.sc) {
+                        tip.show("成功提交！", "亲，现在去看看其他出售吧！");
+                        $("#tipContent").next().text("GO").attr("onclick", "window.location='/sale-list'");
+                    } else {
+                        tip.show("提交失败", result.msg);
+                    }
+                }
+            });
+        }
+    }
+};
--- src/main/webapp/skins/classic/community/sale-publish.ftl
+++ src/main/webapp/skins/classic/community/sale-publish.ftl
@@ -5,6 +5,25 @@
         <@meta title="发需求 - 新助邦"/>
     </head>
     <body>
-       <#include "../common/community-nav.ftl">
+        <#include "../common/community-nav.ftl">
+        <div class="sub-nav">
+            <span><i class="ico-radio-checked"></i>资料</span>
+            <span><i class="ico-radio"></i>答疑</span>
+            <span><i class="ico-radio"></i>授课</span>
+        </div>
+        <div class="wrap">
+            <div class="publish">
+                <input id="title" placeholder="请填写标题名称"/>
+                <textarea id="details" placeholder="亲，请填写具体描述..."></textarea>
+                <div>50</div>
+                <input id="money" placeholder="请填写需要的费用"/>
+                <div>￥(元)</div>
+            </div>
+            <button class="button" onclick="community.salePublish()">提 交</button>
+        </div>
+        <#include "../common/tip.ftl">
+        <script src="/js/lib/jquery-2.1.0.min.js"></script>
+        <script src="/js/common.js"></script>
+        <script src="/js/community.js"></script>
     </body>
 </html>
